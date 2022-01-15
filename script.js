840b05d2921f545fb00d5eb29495940210c7a2bb
Shaun <sandstrom.shaun@gmail.com>
2022-01-15 10:35:02 -0700

    equals function is working

--- script.js
+++ script.js
@@ -6,12 +6,30 @@ function add() {
   addToStack('+');
 }
 
+function equals() {
+  let inputVal = getValue();
+  addToStack(inputVal);
+  if(stack.length >= 3) {
+    let num1 = Number(stack.pop());
+    let operator = stack.pop();
+    let num2 = Number(stack.pop());
+    switch(operator) {
+      case '+':
+        setValue(num1 + num2);
+        break;
+    }
+
+  }
+}
+
+function setValue(val) {
+  document.getElementById('inputNumber').value = val;
+}
+
 function getValue() {
   let inputVal = document.getElementById('inputNumber').value;
   if(!Number.isNaN(inputVal)) {
     return inputVal;
-  } else {
-    return '';
   }
 }
 
